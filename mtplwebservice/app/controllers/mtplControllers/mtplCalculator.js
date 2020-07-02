171562ed365aa8e2b365eeb8888a31bab7f5d5c1
irein-paul <66716541+irein-paul@users.noreply.github.com>
2020-07-02 16:44:14 +0530

    Add files via upload

---
+++ mtplwebservice/app/controllers/mtplControllers/mtplCalculator.js
@@ -0,0 +1,43 @@
+const db = require("../../models/index");
+var dataFormat = require("../../../resources/format/data.json")
+var errorFormat = require("../../../resources/format/error.json")
+const bcrypt = require('bcryptjs');
+const authUtils = require("../../utils/authUtils");
+
+
+function postMtplCalculator (req,res){ 
+    db.mtplCalculator().then(mtpl=>{
+        mtpl.create(req.body).then(data=>{
+            if (data){
+            dataFormat.data = "data uploaded"
+            res.status(200).send(dataFormat)
+            }
+        })
+    }).catch(err => {
+        errorFormat.error = err || "Internal server error"
+        res.status(500).send(errorFormat);
+    })
+}
+
+function getMtplCalculator (req,res) {
+    db.mtplCalculator().then(mtpl=>{
+        mtpl.findOne({ id : req.body.id }).then(data=>{
+            if(data){
+                dataFormat.data = data
+                res.status(200).send(dataFormat)
+            }
+            else{
+                errorFormat.error = "Data not found"
+                res.status(404).send(errorFormat)
+            }
+        })
+    }).catch(err => {
+        errorFormat.error = err || "Internal server error"
+        res.status(500).send(errorFormat);
+    })
+    
+}
+
+module.exports = {postMtplCalculator:postMtplCalculator,
+    getMtplCalculator : getMtplCalculator
+}
---
+++ mtplwebservice/app/controllers/mtplControllers/purchasePolicy.js
@@ -0,0 +1,43 @@
+const db = require("../../models/index");
+var dataFormat = require("../../../resources/format/data.json")
+var errorFormat = require("../../../resources/format/error.json")
+const bcrypt = require('bcryptjs');
+const authUtils = require("../../utils/authUtils");
+
+
+function postPurchasePolicy (req,res){
+    let userData = req.body
+    db.purchasePolicy().then(mtpl=>{
+        mtpl.create(userData).then(data=>{
+            if (data){
+            dataFormat.data = "Data uploaded"
+            res.status(200).send(dataFormat)
+            }
+        })
+    }).catch(err => {
+        errorFormat.error = err || "Internal server error"
+        res.status(500).send(errorFormat);
+    })
+}
+
+function getPurhasePolicy (req,res) {
+    db.purchasePolicy().then(mtpl=>{
+        mtpl.findOne({ id : req.body.id }).then(data=>{
+            if(data){
+                dataFormat.data = data
+                res.status(200).send(dataFormat)
+            }
+            else{
+                errorFormat.error = "Data not found"
+                res.status(404).send(errorFormat)
+            }
+        })
+    }).catch(err => {
+        errorFormat.error = err || "Internal server error"
+        res.status(500).send(errorFormat);
+    })
+}
+
+module.exports = {postPurchasePolicy:postPurchasePolicy,
+    getPurhasePolicy : getPurhasePolicy
+}
