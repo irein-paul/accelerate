0825af06b5cdf2d250753004b1e1f56f43d5141c
irein-paul <66716541+irein-paul@users.noreply.github.com>
2020-07-02 17:07:22 +0530

    Add files via upload

---
+++ mtplwebservice/app/routes/setupRoutes/setupDbroutes.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+var setup = require("../../controllers/setupControllers/setupDb");
+ 
+
+//to post data
+
+router.get('/all', (req, res, next) => {
+    setup.setupdb().then((data)=>{
+        res.json(data)
+    })
+})
+
+module.exports = router; 
